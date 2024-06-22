14c63d8e377d2c46f63c5270d6a792d037298475
Abourass <Abourassa@AssetVal.com>
2024-06-22 15:15:22 -0600

    chore: Update project configuration files and dependencies

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+const tseslint = require('typescript-eslint');
+const { LintGolem } = require('@magik_io/lint_golem');
+
+module.exports = tseslint.config(
+  ...new LintGolem({
+    rootDir: __dirname,
+    tsconfigPaths: ['tsconfig.json'],
+    ignoreGlobs: ['node_modules/**', '*.lock'],
+    disabledRules: ['n/no-extraneous-import', 'n/no-missing-import', 'n/no-require-imports', 'n/hashbang']
+  }).config
+)
